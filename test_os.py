853331dd5b3aa2d94c406bee1821266d511d6fe3
SEAK JIAN DE <57296398+Fred-JD@users.noreply.github.com>
2020-11-24 10:00:53 +0800

    Delete test_os.py

--- test_os.py
+++
@@ -1,6 +0,0 @@
-import os
-
-for file_name in os.listdir('known_faces'):
-    print(file_name)
-    for img_name in os.listdir('known_faces/' + file_name):
-        print(file_name + ' == ' + img_name)
